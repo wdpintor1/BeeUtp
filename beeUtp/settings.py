6b03d92210b8af2cbbc10028856ed78f8171099d
wdpintor1 <160331548+wdpintor1@users.noreply.github.com>
2024-04-07 13:15:17 -0500

    Update settings.py

--- beeUtp/settings.py
+++ beeUtp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4pz+8h)8jo-e5^*6+vi0i_+0g5vfacc**lctfttfg^i!ia0=+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','beeutp-f5aa15bc02e6.herokuapp.com']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'manage',
     'chart'
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,42 +76,47 @@ TEMPLATES = [
 ASGI_APPLICATION = "beeUtp.asgi.application"
 WSGI_APPLICATION = "beeUtp.wsgi.application"
 
-# Channels
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
-}
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'beeUtp',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'beeutp',
+        'USER': 'root',
         'PASSWORD': 'root',
-        'HOST':'localhost', #'database',
-        'PORT':'',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
-}
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.mysql',
-   #     'NAME': 'beeutp',
-    #    'USER': 'root',
-     #   'PASSWORD': 'root',
-      #  'HOST': 'database',
-       # 'PORT': '3306',
-    #}
-#}
+}'''
+# Obtén las variables de entorno proporcionadas por Heroku
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
+# Divide la URL de la base de datos en sus componentes individuales
+if DATABASE_URL:
+    import urllib.parse
+    import psycopg2
 
+    urllib.parse.uses_netloc.append('postgres')
+    url = urllib.parse.urlparse(DATABASE_URL)
 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        }
+    }
+else:
+    # Configuración de base de datos de respaldo si DATABASE_URL no está disponible
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -146,8 +153,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -158,18 +169,5 @@ LOGIN_URL = '/signin/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-#Configuración para Redis
-# Utiliza Redis como backend de cache
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Cambia esto según tu configuración de Redis
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-
-# Utiliza Redis como backend de almacenamiento de sesiones
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Usa la base de datos para almacenar las sesiones
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
